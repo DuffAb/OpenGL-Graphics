19cde666e7542d6b1dda8453764366ccebd5c913
梁德锋 <defeng.liang@3glasses.com>
2020-04-23 17:35:42 +0800

    添加loadCubeMapTexture函数，用于创建天空包围盒纹理

--- glrender/texture.h
+++ glrender/texture.h
@@ -80,6 +80,41 @@ public:
 		return textId;
 	}
 
+	/*
+	* 加载一个cubeMap
+	*/
+	static GLuint loadCubeMapTexture(std::vector<const char*> picFilePathVec,
+		GLint internalFormat = GL_RGB, GLenum picFormat = GL_RGB,
+		GLenum picDataType = GL_UNSIGNED_BYTE, int loadChannels = SOIL_LOAD_RGB)
+	{
+		GLuint textId;
+		glGenTextures(1, &textId);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, textId);	// cubeMap类型
+		GLubyte* imageData = NULL;
+		int picWidth, picHeight;
+		for (std::vector<const char*>::size_type i = 0; i < picFilePathVec.size(); ++i)
+		{
+			int channels = 0;
+			imageData = SOIL_load_image(picFilePathVec[i], &picWidth, &picHeight, &channels, loadChannels);
+			if (imageData == NULL)
+			{
+				std::cerr << "Error::loadCubeMapTexture could not load texture file:" << picFilePathVec[i] << std::endl;
+				return 0;
+			}
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+				0, internalFormat, picWidth, picHeight, 0, picFormat, picDataType, imageData);
+			SOIL_free_image_data(imageData);
+		}
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+
+		return textId;
+	}
+
 #define FOURCC_DXT1 0x31545844 // Equivalent to "DXT1" in ASCII
 #define FOURCC_DXT3 0x33545844 // Equivalent to "DXT3" in ASCII
 #define FOURCC_DXT5 0x35545844 // Equivalent to "DXT5" in ASCII
