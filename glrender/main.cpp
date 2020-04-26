fb6b4f372a2cc0679929d16f4ef6fab345ac5ca8
梁德锋 <defeng.liang@3glasses.com>
2020-04-26 11:40:12 +0800

    折射，refraction-model

--- glrender/main.cpp
+++ glrender/main.cpp
@@ -82,70 +82,14 @@ int main()
 	// 设置视口参数
 	glViewport(0, 0, width, height);
 
-	//Section1 顶点属性数据
-	// 指定立方体顶点属性数据 顶点位置 法向量
-	GLfloat cubeVertices[] = {
-		// 正面
-		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,	// A
-		 0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,	// B
-		 0.5f,  0.5f, 0.5f, 0.0f, 0.0f, 1.0f,	// C
-		 0.5f,  0.5f, 0.5f, 0.0f, 0.0f, 1.0f,	// C
-		-0.5f,  0.5f, 0.5f, 0.0f, 0.0f, 1.0f,	// D
-		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,	// A
-
-		// 背面
-		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,	// E
-		-0.5f,  0.5f, -0.5f, 0.0f, 0.0f, -1.0f, // H
-		 0.5f,  0.5f, -0.5f, 0.0f, 0.0f, -1.0f,	// G
-		 0.5f,  0.5f, -0.5f, 0.0f, 0.0f, -1.0f,	// G
-		 0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,	// F
-		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,	// E
-
-		// 左侧面
-		-0.5f,  0.5f,  0.5f, -1.0f, 0.0f, 0.0f,	// D
-		-0.5f,  0.5f, -0.5f, -1.0f, 0.0f, 0.0f, // H
-		-0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f, // E
-		-0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f,	// E
-		-0.5f, -0.5f,  0.5f, -1.0f, 0.0f, 0.0f,	// A
-		-0.5f,  0.5f,  0.5f, -1.0f, 0.0f, 0.0f,	// D
-
-		// 右侧面
-		0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,	// F
-		0.5f,  0.5f, -0.5f, 1.0f, 0.0f, 0.0f,	// G
-		0.5f,  0.5f,  0.5f, 1.0f, 0.0f, 0.0f,	// C
-		0.5f,  0.5f,  0.5f, 1.0f, 0.0f, 0.0f,	// C
-		0.5f, -0.5f,  0.5f, 1.0f, 0.0f, 0.0f,	// B
-		0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,	// F
-
-		// 顶面
-		 0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f,	// G
-		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f,   // H
-		-0.5f, 0.5f,  0.5f, 0.0f, 1.0f, 0.0f,	// D
-		-0.5f, 0.5f,  0.5f, 0.0f, 1.0f, 0.0f,	// D
-		 0.5f, 0.5f,  0.5f, 0.0f, 1.0f, 0.0f,	// C
-		 0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f,	// G
-
-		// 底面
-		-0.5f, -0.5f,  0.5f, 0.0f, -1.0f, 0.0f,	// A
-		-0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f, // E
-		 0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f,	// F
-		 0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f,	// F
-		 0.5f, -0.5f,  0.5f, 0.0f, -1.0f, 0.0f,	// B
-		-0.5f, -0.5f,  0.5f, 0.0f, -1.0f, 0.0f,	// A
-	};
-	GLuint cubeVAOId, cubeVBOId;
-	glGenVertexArrays(1, &cubeVAOId);
-	glGenBuffers(1, &cubeVBOId);
-	glBindVertexArray(cubeVAOId);
-	glBindBuffer(GL_ARRAY_BUFFER, cubeVBOId);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
-	// 顶点位置数据
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
-	glEnableVertexAttribArray(0);
-	// 顶点法向量数据
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GL_FLOAT)));
-	glEnableVertexAttribArray(1);
-	glBindVertexArray(0);
+	//Section1 加载模型数据
+	Model objModel;
+	if (!objModel.loadModel("resources/models/sphere/sphere.obj"))
+	{
+		glfwTerminate();
+		std::system("pause");
+		return -1;
+	}
 
 	// Section2 创建Skybox
 	std::vector<const char*> faces;
@@ -166,8 +110,8 @@ int main()
 	skybox.init(faces);
 
 	// Section3 准备着色器程序
-	Shader shader("shader/environmentMapping/refraction/scene.vertex", "shader/environmentMapping/refraction/scene.frag");
-	Shader skyBoxShader("shader/environmentMapping/refraction/skybox.vertex", "shader/environmentMapping/refraction/skybox.frag");
+	Shader shader("shader/environmentMapping/refraction-model/scene.vertex", "shader/environmentMapping/refraction-model/scene.frag");
+	Shader skyBoxShader("shader/environmentMapping/refraction-model/skybox.vertex", "shader/environmentMapping/refraction-model/skybox.frag");
 
 	glEnable(GL_DEPTH_TEST);	// 开启深度测试
 	glEnable(GL_CULL_FACE);		// 开启面剔除
@@ -196,12 +140,12 @@ int main()
 		shader.updateUniformMatrix4fv("projection", 1, GL_FALSE, glm::value_ptr(projection));
 		shader.updateUniformMatrix4fv("view", 1, GL_FALSE, glm::value_ptr(view));
 		shader.updateUniformMatrix4fv("model", 1, GL_FALSE, glm::value_ptr(model));
-		glBindVertexArray(cubeVAOId);
+		
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_CUBE_MAP, skybox.getTextId());
 		shader.updateUniform1i("envText", 0);
 		shader.updateUniform3f("cameraPos", camera.position.x, camera.position.y, camera.position.z); // 注意设置观察者位置
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		objModel.draw(shader);	// 绘制球体
 
 		// 然后绘制包围盒
 		skyBoxShader.use();
@@ -219,8 +163,6 @@ int main()
 	}
 
 	// 释放资源
-	glDeleteVertexArrays(1, &cubeVAOId);
-	glDeleteBuffers(1, &cubeVBOId);
 	glfwTerminate();
 	return 0;
 }
